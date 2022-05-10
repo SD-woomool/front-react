c7d4b637544669400cd61e0cecf076b8154568ad
Tory <88531407+1two13@users.noreply.github.com>
2022-05-10 22:07:50 +0900

    features: 네이버 지도 마커 & 글작성 컴포넌트 뼈대 작업

--- src/components/Write.tsx
+++ src/components/Write.tsx
@@ -2,11 +2,43 @@ import React from "react";
 import styled from "styled-components";
 
 const Body = styled.div`
-  background-color: red;
+  background-color: pink;
+  height: 100%;
 `;
 
 function Write() {
-  return <Body>테스트</Body>;
+  return (
+    <Body>
+      <div>
+        <button>버튼아이콘</button>
+        <span>코스</span>
+      </div>
+      <div>코스 제목을 입력해주세요.</div>
+      <div>네이버 지도 들어갈 공간</div>
+      <div>
+        <div>코스</div>
+        <div>아직 추가된 장소가 없습니다! 추가해주세요!</div>
+        <button>장소추가</button>
+      </div>
+      <div>
+        <div>설명</div>
+        <div>코스에 대한 설명을 적어주세요.</div>
+      </div>
+      <div>
+        <span>교통수단</span>
+        <button>대중교통</button>
+        <button>자동차</button>
+      </div>
+      <div>
+        <span>태그</span>
+        <span>(선택)</span>
+        <div>
+          <button>+</button>
+        </div>
+      </div>
+      <button>작성완료</button>
+    </Body>
+  );
 }
 
 export default Write;
--- src/components/maps/NaverMap.tsx
+++ src/components/maps/NaverMap.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useEffect } from "react";
 import styled from "styled-components";
+import cafeSvg from "../../assets/cafe.svg";
 
 const Map = styled.div`
   width: 100%;
   height: calc(100vh - 130px);
+  :focus {
+    outline: none;
+  }
 `;
 
 function NaverMap() {
@@ -15,6 +19,22 @@ function NaverMap() {
         center: new naver.maps.LatLng(37.511337, 127.012084),
         zoom: 13,
       });
+      // cafe 마커 예시 => 현재는 최초 렌더링 시 실행되는 코드 블럭 안에 넣어두었지만, 위치 이동시켜야 함
+      var cafeMarker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(37.363777, 127.1174332),
+        map: map,
+        icon: {
+          content: `
+            <div id="content" style="background:#ffffff;width:24px;height:24px;border:3px solid #387FEA;border-radius:50%;position:absolute;top:-2px;left:-1.5px;display:flex;justify-content:center;align-items:center;">
+              <img style="width:16px;" src="${cafeSvg}">
+            </div>
+            <svg width="27" height="36" viewBox="0 0 18 24" fill="#387FEA" xmlns="http://www.w3.org/2000/svg"><path d="M8.99988 0C4.20103 0 0.325928 3.8751 0.325928 8.67395C0.325928 10.3472 0.860424 11.9448 1.60407 13.2578L7.25695 23.0413C7.61135 23.6804 8.32595 23.9999 8.99988 23.9999C9.67387 23.9999 10.3535 23.6804 10.7428 23.0413L16.3957 13.2636C17.1451 11.9505 17.6738 10.3819 17.6738 8.67964C17.6738 3.88091 13.7987 0 8.99988 0ZM8.99988 11.236C7.25695 11.236 5.83357 9.81266 5.83357 8.06974C5.83357 6.32681 7.25695 4.90343 8.99988 4.90343C10.7428 4.90343 12.1662 6.32681 12.1662 8.06974C12.1662 9.81847 10.7428 11.236 8.99988 11.236Z"></path></svg>
+          `,
+          size: new naver.maps.Size(38, 58),
+          anchor: new naver.maps.Point(19, 58),
+        },
+      });
+      console.log(cafeSvg);
     };
     initMap();
   }, []);
